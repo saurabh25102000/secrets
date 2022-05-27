dd33810dba75f7acbb205753ba3258f1601ca087
SKRAJ <saurabh2002@iitg.ac.in>
2022-05-27 08:01:27 +0530

    Level 3: Hashing password

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+// const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 const app = express()
 
@@ -21,9 +22,9 @@ const userSchema = new mongoose.Schema ({
   password: String
 })
 
-console.log(process.env.API_KEY)
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
 
+// md5(message) to convert message into hash
 const User = new mongoose.model("User", userSchema)
 
 app.get("/", (req, res)=>{
@@ -41,7 +42,7 @@ app.get("/register", (req, res)=>{
 app.post("/register", (req, res)=>{
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   newUser.save((err)=>{
@@ -55,7 +56,7 @@ app.post("/register", (req, res)=>{
 
 app.post("/login", (req, res)=>{
   const username = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
 
   User.findOne({email: username}, (err, foundUser)=>{
     if(err){
